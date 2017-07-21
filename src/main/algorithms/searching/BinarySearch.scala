640da7cd244a400cc235335ab5a89f7199305b98
Cigith Pillai <cigith.pillai@one.verizon.com>
2017-07-21 15:18:51 -0700

    Binary Search

--- src/main/algorithms/searching/BinarySearch.scala
+++ src/main/algorithms/searching/BinarySearch.scala
@@ -2,7 +2,37 @@ package main.algorithms.searching
 
 /**
   * Created by pillaci on 7/16/17.
+  *
+  * Assumption : Input array is already sorted
   */
-object BinarySearch {
+object BinarySearch extends App {
+
+
+  def binarySearch(list: Array[Int], searchValue: Int): Boolean = {
+    def bsf(list: Array[Int], searchValue: Int, start: Int, end: Int) : Boolean = {
+      if(start > end) return false
+      val mid = start + (end - start + 1)/2
+      list match {
+        case (xs: Array[Int]) if xs(mid) == searchValue => return true
+        case (xs: Array[Int]) if xs(mid) > searchValue => bsf(list, searchValue, start, mid - 1)
+        case (xs: Array[Int]) if xs(mid) < searchValue => bsf(list, searchValue, mid + 1, end)
+      }
+    }
+    bsf(list, searchValue, 0, list.length - 1)
+  }
+
+  def binarySearchRecursive(list: Array[Int], searchValue: Int)(start: Int = 0, end: Int = list.length - 1) : Boolean = {
+    if(start > end) return false
+    val mid = start + (end - start + 1)/2
+    if (list(mid) == searchValue) return true
+    else if (list(mid) > searchValue) binarySearchRecursive(list, searchValue)(start, mid - 1)
+    else binarySearchRecursive(list, searchValue)(mid + 1, end)
+  }
+
+  val inputList = Array(1,2,3,4,5,6,7,8,9,10)
+  println(s"Input List : $inputList")
+  println(s"Search :: ${binarySearch(inputList, 5)}")
+  println(s"Search :: ${binarySearchRecursive(inputList, 5)_}")
+
 
 }
